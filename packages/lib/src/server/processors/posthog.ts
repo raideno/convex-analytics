9bf905d896457d4e94ecb54826fabc837cf4aecc
raideno56 <nadirkichou@hotmail.fr>
2025-11-27 14:17:14 +0100

    fix: correct posthog processor
    remove hardcoded key & rotate it on posthog dashboard

--- packages/lib/src/server/processors/posthog.ts
+++ packages/lib/src/server/processors/posthog.ts
@@ -11,25 +11,29 @@ export const PosthogProcessorFactory = (
   return {
     events: args.events,
     handler: async (_, events) => {
-      await fetch(`${args.host}/batch/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${"phc_dfwoZU5kkU9T7R7hOdUbbrX9P70FPndypLt2Ym8NrR3"}`,
-        },
-        body: JSON.stringify(
-          events.map((event) => ({
-            event: event.name,
-            distinct_id: event.distinctId,
-            properties: {
-              ...event.properties,
-            },
-            timestamp: new Date(event._creationTime).toISOString(),
-          }))
-        ),
-      });
+      try {
+        await fetch(`${args.host}/batch/`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            api_key: args.key,
+            batch: events.map((event) => ({
+              event: event.name,
+              distinct_id: event.distinctId,
+              properties: {
+                ...event.properties,
+              },
+              timestamp: new Date(event._creationTime).toISOString(),
+            })),
+          }),
+        });
 
-      return events.map((e) => e._id);
+        return events.map((e) => e._id);
+      } catch (error) {
+        return [];
+      }
     },
   };
 };
